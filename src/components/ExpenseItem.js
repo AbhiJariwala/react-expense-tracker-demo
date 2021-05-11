383d3aea2eac50498d89c82a3f8ae4a34756ba66
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-05-11 23:01:38 +0530

    using props to pass data between one component to another

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-
-  const expenseDate = new Date(2021, 4, 11);
-  const expenseTitle = 'Sample Title';
-  const expenseAmount = 234.45;
+function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
