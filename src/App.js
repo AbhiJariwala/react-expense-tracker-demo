92604bf1e9a07be432682f1c654d6ed7d7ec65b6
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-05-11 23:59:45 +0530

    splitting code into multiple components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
-  const expense = [
+  const expenses = [
     {id: 'e1', title: 'New TV', amount: 1099.50, date: new Date(2021, 7, 19)},
     {id: 'e2', title: 'New Computer', amount: 2500, date: new Date(2020, 1, 28)},
     {id: 'e3', title: 'New Laptop', amount: 699, date: new Date(2021, 6, 30)},
@@ -12,10 +12,7 @@ function App() {
   return (
     <div className="App">
       React Demo
-      <ExpenseItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date}></ExpenseItem>
-      <ExpenseItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date}></ExpenseItem>
-      <ExpenseItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date}></ExpenseItem>
-      <ExpenseItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date}></ExpenseItem>
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
