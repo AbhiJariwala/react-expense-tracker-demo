5b4fdba0db0cda7ce44b3dfc27e79b411f0186bd
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-05-11 22:38:07 +0530

    using expression syntax to display dynamic values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2021, 4, 11);
+  const expenseTitle = 'Sample Title';
+  const expenseAmount = 234.45;
+
   return (
     <div className="expense-item">
-      <div>11 May 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>Sample Title</h2>
-        <div className="expense-item__price">$234.45</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
