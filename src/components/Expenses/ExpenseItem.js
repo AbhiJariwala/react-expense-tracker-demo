26cd6d2e303e752c0e1ccf00c789c73134b8c838
Abhishek Jariwala <abhishekjari15@gmail.com>
2021-05-12 23:11:11 +0530

    using useState hook to update values in DOM

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import {useState} from "react";
 
-const ExpenseItem = props => (
-  <Card className="expense-item">
-    <ExpenseDate date={props.date}/>
-    <div className="expense-item__description">
-      <h2>{props.title}</h2>
-      <div className="expense-item__price">${props.amount}</div>
-    </div>
-  </Card>
-);
+const ExpenseItem = props => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const titleHandler = () => {
+    setTitle('Updated!!');
+    console.log('title changed');
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={titleHandler}>Update Title</button>
+    </Card>
+  )
+};
 
 export default ExpenseItem;
